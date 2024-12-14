30ea776d3cb0ff52b4f712f5c52f5a3ffc320920
Kisari <seikakukisari@gmail.com>
2024-12-14 15:51:03 +0700

    Initialize the project structure

---
+++ Currency-Converter/Currency-Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency-Converter
+//
+//  Created by Minh Trương on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
